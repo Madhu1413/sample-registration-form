cf076b1279184291951d7d0d12e8266d77602bcb
David Rivers <david@dpg.me>
2015-04-04 19:38:46 -0400

    Fix positioning bug with persisted-placeholder; add check mark for :valid

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,7 +1,7 @@
 // Depends on jquery.js, contain-inputs.js, and part of form.less
 
 $(function() {
-	$('.js-validate input[required]:not(:submit)').parent('.input-container').append('<span class="check-mark">YES!</span>');
+	$('.js-validate input[required]:not(:submit)').parent('.input-container').append('<span class="check-mark">&nbsp;&#10003;&nbsp;</span>');
 
 	// Initialize validatable inputs with .validate class after user starts using form
 	// Necessary in order to not cause :validate and :invalidate styles to trigger before user has had a chance to type in any inputs
